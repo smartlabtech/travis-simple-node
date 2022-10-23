c8e6b42e6921bde5a330efc2b6a57e6d3c9e9d29
smartlabtech <michaelyoussif.elias@gmail.com>
2022-10-23 15:15:42 +0200

    Enhancements

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-
+var FAVORIT_INTRUCTOR = process.env.NAME
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
-    await sleep(5000);
+  while (true) {
+    console.log(`${FAVORIT_INTRUCTOR} Microservices rock!`)
+    await sleep(5000)
   }
 }
 
-main();
+main()
